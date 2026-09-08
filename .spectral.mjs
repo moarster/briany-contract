77c637c9be47b3f0d219cea57c6e4af4ed1733f1
moarster <ivan@moarse.ru>
2026-09-08 15:24:33 +0300

    Modified 2 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjMyMDI0OA/spectral.js?branch_slug=master&token=589da055-b2ec-4805-acbd-a71a17b91da8";
+export default { extends: ruleset };
